a4f51935912e796e057823722140d35b3345cc1e
Gaurab sapkota <iamgaurabsap20@gmail.com>
2023-08-12 16:28:47 +0545

    add cors

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ import route from "./routes/userRoutes.js";
 import Connectdb from "./database/Db.js";
 import dotenv from "dotenv";
 import cookieparser from "cookie-parser";
-// import { ErrorHandle } from "./middleware/errorHandle.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1", route);
 app.use(cookieparser());
-// app.use(ErrorHandle);
+
 dotenv.config();
 app.listen(4000, () => {
   Connectdb();
