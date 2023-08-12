dea54742ab3b400bb48eff1cc45660adc8049dc6
Gaurab sapkota <iamgaurabsap20@gmail.com>
2023-08-12 15:25:09 +0545

    gb

--- middleware/errorHandle.js
+++
@@ -1,9 +0,0 @@
-export const ErrorHandle = async (error, req, res, next) => {
-  //   console.log("hit");
-  const statusCode = res.statusCode ? res.statusCode : 500;
-  //   console.log(statusCode);
-  return res.status(statusCode).json({
-    message: error.message,
-    stack: process.env.MY_ENV === "development" ? error.stack : null,
-  });
-};
